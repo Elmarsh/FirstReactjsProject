a57bca9136e0edf79bc6ec4aa55dce317f9827b3
Elmar Abdullayev <elmarsha@code.edu.az>
2021-05-04 20:57:47 +0400

    updated project
    created: Emloyee, EmloyeeList  Component;
     index.css updated

---
+++ reactapp/src/components/Employee.js
@@ -0,0 +1,14 @@
+
+const Employee = () =>{
+    return(
+        <ul>
+            <li>Employee</li>
+            <li>Employee</li>
+            <li>Employee</li>
+        </ul>
+    )
+}
+
+
+
+export default Employee;
